823d3608d949c72834bc5918a20277ba6f8f88de
Jonathan Lisic <jlisic@gmail.com>
2013-02-07 05:03:31 -0500

    Setup for multi-level simulation

--- simCrop.R
+++ simCrop.R
@@ -1,5 +1,12 @@
-library(raster)
 
+set.seed(1)
+
+library(raster)
+library(rgdal)
+library(clv)
+
+source('~/src/simCrop/floodFill.R')
+source('~/src/simCrop/smooth.R')
 
 # simCrop.R
 
@@ -10,6 +17,9 @@ library(raster)
 # Half-Quarter Section - 80
 # Quarter-Quarter Section - 40
 # assume approximately 56mx56m pixels so that one quarter section contains 49 pixels
+
+# cropscape projection
+aeaString <-"+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs +towgs84=0,0,0"
 
 # function to convert quarter sections to half quarter sections
 # x value
@@ -181,16 +191,12 @@ simCrop.pixelParcelUpdate <- function(b,p,K,errorRate) {
   # first need to subset by ownership
   nOwn <- unique(bValue[,'own'])
 
-nOwn <<- nOwn
-
   for( i in 1:length(nOwn)) {
     # get the subset for a particular ownership
     subSetOwn <- which( bValue[,'own'] == nOwn[i] ) 
     #calculate next iteration and write it to the data set
     bValue[subSetOwn,'value'] <- unlist(aggregate( bValue[subSetOwn,'value'], list(bValue[subSetOwn,'object']), function(x) generateCropTypes(x,p,K[[i]]))$x)
   }
-
-bValue <<- bValue
 
   bValue[,'error'] <- unlist(aggregate( bValue[,'value'], list(bValue[,'object']), function(x) generateErrors(x,p,errorRate))$x)
 
@@ -200,6 +206,16 @@ bValue <<- bValue
 
 }
 
+
+simCrop.pixelParcelChangeError <- function(b,p,errorRate) {
+  bValue <- b$value
+
+  bValue <- bValue[sort( bValue[,'object'], index.return=T )$ix,]
+  bValue[,'error'] <- unlist(aggregate( bValue[,'value'], list(bValue[,'object']), function(x) generateErrors(x,p,errorRate))$x)
+  b$value <- bValue[sort( bValue[,'pixel'],index.return=T)$ix,]
+
+  return(b)
+}
 
 # function to add owner 
 simCrop.objectOwner <- function(b,p.own) {
@@ -250,6 +266,9 @@ K2 <- matrix( c( .98, .005, .005, .01,
 
 K <- list(K1,K2)
 
+
+
+
 a <- simCrop.partitionPLSS(6,6)
 b <- simCrop.pixelParcel(a)
 
@@ -260,26 +279,89 @@ b <- simCrop.objectOwner(b,p.own)
 for(i in 1:10){
 
   if( i == 1 ) {
-    b.list       <- list( simCrop.pixelParcelUpdate(b,p,K,.05) )
-    r.error.list <- list( simCrop.pixelParcelToRaster(b.list[[1]],'error') )
+    b.list       <- list( simCrop.pixelParcelUpdate(b,p,K,.01) )
+    r.list       <- list( simCrop.pixelParcelToRaster(b.list[[i]],'value') )
+    projection(r.list[[i]]) <- CRS(aeaString)
   } 
   else {
-    b.list[[i]]        <- simCrop.pixelParcelUpdate(b.list[[i-1]],p,K,.05)
-    r.error.list[[i]] <- simCrop.pixelParcelToRaster(b.list[[i]],'error')
+    b.list[[i]]        <- simCrop.pixelParcelUpdate(b.list[[i-1]],p,K,.01)
+    r.list[[i]] <- simCrop.pixelParcelToRaster(b.list[[i]],'value')
+    projection(r.list[[i]]) <- CRS(aeaString)
   } 
 
-  #x11()
-  #plot(r.error.list[[i]], main=sprintf("t=%d",i) )
 }
 
-r <- brick( unlist(r.error.list) )
-
-  x11()
-  plot(r)
 
 
+#generate errors
+
+runSim <- function( errorVal ) {
+  j <- 1 # set initial index for sim
+
+  for( h in c(1, 2, 3, 10) ) {  # iterate over years
+    for( s in c(3,7,11,15) ) {  # iterate over x by x matrices 
+
+      print( sprintf("h = %d, s = %d", h, s))  
+      for( i in 1:10 ){
+        if( i == 1 ) {
+          b.temp <- simCrop.pixelParcelChangeError(b.list[[i]],p,errorVal)
+          r.error.list <- list( simCrop.pixelParcelToRaster(b.temp,'error') )
+          projection(r.error.list[[i]]) <- CRS(aeaString)
+        } 
+        else {
+          b.temp <- simCrop.pixelParcelChangeError(b.list[[i]],p,errorVal)
+          r.error.list[[i]] <- simCrop.pixelParcelToRaster(b.temp,'error')
+          projection(r.error.list[[i]]) <- CRS(aeaString)
+        } 
+      }
+      
+      
+      
+      
+      
+      # create raster objects
+      r.error <- brick( unlist(r.error.list) )
+      r <- brick( unlist(r.list) )
+      
+      
+      
+      # we now get a new object index since the prior index my violate our parcel definition 
+      r.object <- floodFill.raster(r) 
+      #print(sprintf("New Object No. %d from %d", max(r.object), length(unique(b.list[[1]]$value[,'object']))))
+      
+      
+      # run the kernel smoother on the item with errors
+      r.smooth <- r.error
+      values(r.smooth) <- localMode(r.error,h,s)
+      
+      # get the mse
+      r.mse <- mean((values(r.smooth) - values( r ))^2)
+      
+      # calculate rand and other stats
+      r.smooth.object <- floodFill.raster(r.smooth)
+      
+      r.compare <- std.ext(as.vector(r.smooth.object), as.vector(r.object) )
+      
+      r.compare[5] <- r.mse
+      r.compare[6] <- errorVal 
+      r.compare[7] <- h 
+      r.compare[8] <- s 
+      
+      names(r.compare) <- c( names(r.compare)[1:4], "mse", "error", "h", "s")
 
 
+      if( j > 1) {
+        r.compare.all[[j]] <- r.compare
+      }
+      else {
+        r.compare.all <- list(r.compare)
+      }
+
+     j <- j + 1 
+    }
+  }
+
+} 
 
 
 
--- smooth.R
+++ smooth.R
@@ -26,47 +26,73 @@
 localMode <- function( r, h, s) {
 
   a <- getValuesFocal(r,ngb=s)
-  
+
+  layers <- nlayers(r)
+
   # a is a list where each element is pixels x focal
   # so what we want to do is to grab our first set and find what is the most popular
-  a.return.values <- matrix(0, nrow=ncell(r), ncol=length(a))
+  a.return.values <- matrix(0, nrow=ncell(r), ncol=layers)
 
   for(i in 1:ncell(r)) { 
   #for(i in 1:1000) { 
 
     a.hood <- c()
-    for(j in 1:length(a)) {
-    
-      a.hood <- rbind(a.hood, a[[j]][i,] ) 
-    
+   
+    if( layers > 1 ) { 
+      for(j in 1:layers) a.hood <- rbind(a.hood, a[[j]][i,] ) 
+    } else { 
+      a.hood <- matrix(a[i,],nrow=1)
     }
+#    print(sprintf(" ------------- %d ------------- ",i)) 
+#    print("a.hood")
+#    print(a.hood) 
   
     # run the filter over each year
-    for( j in 1:length(a)) {
+    for( j in 1:layers) {
 
       # get the index of our response value
       h0 <- min(j,h+1)
   
       
       #only get years within the window 
-      time.window <- max(1, j - h):min( length(a), j + h) 
+      time.window <- max(1, j - h):min( layers, j + h) 
   
       
       # so this mess really just gets the non NA neighborhood, and classes within the spatial domain together.
       # we transpose each of these classes so we can use aggregate to determine the most popular classes
-      a.work <- data.frame(cbind(1, t(a.hood[time.window,!is.na(a.hood[1,])])))
-      
+      if(layers > 1) {
+        a.work <- data.frame(cbind(1, t(a.hood[time.window,!is.na(a.hood[1,])])))
+      } else {
+        a.work <- data.frame(cbind(1, matrix( a.hood[time.window,!is.na(a.hood[1,])], ncol=1  )))
+      }
+#      print("a.work")
+#      print(a.work) 
+     
       a.results <- aggregate( X1 ~ . , a.work, FUN=length)
       
       a.freq <- a.results$X1
       
+#      print("a.freq")
+#      print(a.freq) 
+      
       a.return <- a.results[which( a.freq == max(a.freq) ),1:length(time.window)] 
       
-      if(nrow(a.return) > 1) {
-        a.return <- a.return[sample(1:nrow(a.return),size=1),]
+#      print("a.return - initial")
+#      print(a.return) 
+      
+      if( layers > 1) { 
+        if(nrow(a.return) > 1) {
+          a.return <- a.return[sample(1:nrow(a.return),size=1),]
+        }
+        a.return.values[i,j] <- a.return[1,h0]
+      } else {
+        if(length(a.return) > 1) {
+          a.return <- a.return[sample(1:length(a.return),size=1)]
+        }
+        a.return.values[i,j] <- a.return
       }
-  
-      a.return.values[i,j] <- a.return[1,h0]
+#      print("a.return - final")
+#      print(a.return) 
     }
   }
 
@@ -76,19 +102,19 @@ localMode <- function( r, h, s) {
   
 
 
-runTime <- proc.time()
-
-h <- 3 
-s <- 11
-
-smoothValues <- localMode(r,h,s)
-
-print("runtime:")
-print(proc.time() - runTime)
-
-x11()
-r.new <- r
-values(r.new) <- smoothValues
-title(sprintf("h=%d, s=%d",h,s))
-
+#runTime <- proc.time()
+#
+#h <- 3 
+#s <- 11
+#
+#smoothValues <- localMode(r,h,s)
+#
+#print("runtime:")
+#print(proc.time() - runTime)
+#
+#x11()
+#r.new <- r
+#values(r.new) <- smoothValues
+#title(sprintf("h=%d, s=%d",h,s))
+#
 
